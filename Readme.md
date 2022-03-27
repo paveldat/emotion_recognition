85e2631e77a0ee79567c35ba3b80880d46a47e10
Pavel Dat <67552033+paveldat@users.noreply.github.com>
2022-03-27 15:01:12 +0300

    face1

---
+++ Readme.md
@@ -0,0 +1,34 @@
+# Gesture volume control
+In this project I will analyze how you can find out age, gender, race and emotion from a photo.
+
+## How to install
+1. Clone this repository on your computer
+`https://github.com/paveldat/emotion_recognition.git`
+2. Install all the requirements
+`run libraries.bat`
+3. Run the program
+`python main.py`
+4. Write path to the image you want to analyze and press ENTER
+
+## Result
+<img src="https://github.com/paveldat/emotion_recognition/blob/main/img/1.jpg">
+
+```
+Age: 24
+Gender: Man
+Race:
+asian - 0.46%
+indian - 1.3%
+black - 0.55%
+white - 61.98%
+middle eastern - 8.23%
+latino hispanic - 27.47%
+Emotions:
+angry - 85.94%
+disgust - 0.0%
+fear - 13.97%
+happy - 0.0%
+sad - 0.08%
+surprise - 0.0%
+neutral - 0.01%
+```
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from deepface import DeepFace
 import json
 
-def analyze_face_emotions():
+def analyze_face_emotions(path_to_image):
 	try:
-		result = DeepFace.analyze(img_path='img/1.jpg', actions=['age', 'gender', 'race', 'emotion'])
+		result = DeepFace.analyze(img_path=path_to_image, actions=['age', 'gender', 'race', 'emotion'])
 		
 		print(f'Age: {result.get("age")}')
 		print(f'Gender: {result.get("gender")}')
@@ -25,7 +25,12 @@ def analyze_face_emotions():
 		return ex
 
 def main():
-	analyze_face_emotions()
+	print("Please, type the name of the file. Or press enter to skip. Default name is /img/1.jpg\n")
+	path_to_image = input()
+	if not path_to_image:
+		path_to_image = 'img/1.jpg'
+
+	analyze_face_emotions(path_to_image)
 
 if __name__ == '__main__':
 	main()
